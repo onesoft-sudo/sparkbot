42679e939af58eba0bea0d81f78d2aa529716de2
Ar Rakin <rakinar2@gmail.com>
2022-10-31 22:54:43 +0600

    fix: pre-release hook

--- .github/pre-release.js
+++ .github/pre-release.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 exports.preCommit = ({ version }) => {
   const file = path.resolve(__dirname, "..", "gradle.properties");
-  const contents = fs.readFileSync(file);
+  const contents = fs.readFileSync(file).toString();
   
   contents.replace(/version\=([^\n]+)/gi, `version=${version}`);
   
