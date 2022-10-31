591303c93e24ca23ecccb5d5cee4e149408a25f2
Ar Rakin <rakinar2@gmail.com>
2022-10-31 17:22:09 +0000

    chore: remove unneeded files

--- .github/pre-changelog.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-const file = path.resolve(__dirname, "..", "gradle.properties");
-const contents = fs.readFileSync(file).toString();
-const version = contents.match(/version\=([^\n]+)/gi)[0].split('=')[1];
-
-fs.writeFileSync(path.join(__dirname, "..", ".version.json"), `{"version":"${version}"}`);
