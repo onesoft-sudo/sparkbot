c998abe169d57627c639b553489c940c29b0219b
Ar Rakin <rakinar2@gmail.com>
2022-10-31 15:15:01 +0000

    feat: add about command

--- app/src/main/kotlin/org/onesoftnet/spark/commands/AboutCommand.kt
+++ app/src/main/kotlin/org/onesoftnet/spark/commands/AboutCommand.kt
@@ -2,8 +2,12 @@ package org.onesoftnet.spark.commands;
 
 import org.onesoftnet.spark.CommandPayload;
 import org.onesoftnet.spark.Command;
+import org.onesoftnet.spark.App;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.EmbedBuilder;
+import java.awt.Color;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 class AboutCommand : Command() {
     override val name = "about";
@@ -11,8 +15,16 @@ class AboutCommand : Command() {
     override val metaData = Commands.slash(name, description);
 
     override fun run(payload: CommandPayload) {
+        val embed = EmbedBuilder();
+
+        embed.setAuthor("SparkBot", null, App.app!!.jda!!.getSelfUser().getAvatarUrl());
+        embed.setDescription("A bot for issue tracking.");
+
+        val builder = MessageCreateBuilder()
+            .setEmbeds(embed.build());
+
         if (payload.isSlashCommand()) {
-            payload.commandEvent!!.reply("Pong!").queue();
+            payload.commandEvent!!.reply(builder.build()).queue();
         }
     }
 }
