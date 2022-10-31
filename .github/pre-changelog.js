ae36d8d901119d4ccec1c64b4161dc7af447be39
Ar Rakin <rakinar2@gmail.com>
2022-10-31 17:09:56 +0000

    fix: workflows

--- .github/pre-changelog.js
+++ .github/pre-changelog.js
@@ -5,11 +5,4 @@ const file = path.resolve(__dirname, "..", "gradle.properties");
 const contents = fs.readFileSync(file).toString();
 const version = contents.match(/version\=([^\n]+)/gi)[0].split('=')[1];
 
-exports.preVersionGeneration = () => {
-    fs.writeFileSync(path.join(__dirname, "..", ".version.json"), `{"version":"${version}"}`);
-    return version;
-}
-
-exports.preTagGeneration = (tag) => {
-    return `v${version}`;
-}
+fs.writeFileSync(path.join(__dirname, "..", ".version.json"), `{"version":"${version}"}`);
