f5e0f6abfe504d96832a08ac787c4f0e102a3bd4
Ar Rakin <rakinar2@gmail.com>
2022-10-31 17:05:11 +0000

    fix: workflows

--- .github/pre-changelog.js
+++ .github/pre-changelog.js
@@ -6,6 +6,7 @@ const contents = fs.readFileSync(file).toString();
 const version = contents.match(/version\=([^\n]+)/gi)[0].split('=')[1];
 
 exports.preVersionGeneration = () => {
+    fs.writeFileSync(path.join(__dirname, "..", ".version.json"), `{"version":"${version}"}`);
     return version;
 }
 
